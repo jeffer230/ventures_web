690391a617c10f0b07969bd6fece0c125e010ad9
Jefferson <jeffer230@gmail.com>
2024-12-03 21:19:45 -0500

    emprendimineto relation

--- src/app/components/emprendimiento/emprendimiento.component.ts
+++ src/app/components/emprendimiento/emprendimiento.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../services/service.service';
-import { Category, Venture,municipality } from '../../interfaces/interface';
+import { SwalService } from '../../services/swal.service';
+import { Venture,municipality,Category,User,Event,UsuarioEmprendimiento,EmprendimientoEvento } from '../../interfaces/interface';
 import { NgForm } from '@angular/forms';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-emprendimiento',
@@ -13,17 +15,25 @@ export class EmprendimientoComponent implements OnInit{
   emprendimientos: Venture[] = [];
   municipios: municipality[] = [];
   categorias: Category[] = [];
+  usuarios: User[] = [];
+  eventos: Event[] = [];
   selectedEmprendimiento: Venture = {} as Venture;
+  emprendimientoUsuario: UsuarioEmprendimiento = {} as UsuarioEmprendimiento;
+  emprendimientoEvento: EmprendimientoEvento = {} as EmprendimientoEvento;
+  selectedUsuarios: number[] = [];
+  selectedEventos: number[] = [];
   isModalOpen = false;
   isLoading: boolean = true;
   modalEditar: boolean = false;
 
-  constructor(private service: ServiceService) {}
+  constructor(private service: ServiceService, private swal: SwalService) {}
 
   ngOnInit(): void {
     this.consultarEmprendimientos();
     this.consultarMunicipios();
     this.consultarCategorias();
+    this.consultarUsuarios();
+    this.consultarEventos();
   }
 
   // Consulta la lista de todos los emprendimientos
@@ -35,8 +45,9 @@ export class EmprendimientoComponent implements OnInit{
         this.isLoading = false;
       },
       error: (err) => {
-        console.error('Error al obtener los emprendimientos:', err);
+        console.error('Error al consultar los emprendimientos:', err);
         this.isLoading = false;
+        this.swal.error('Error', 'Hubo un error al consultar los usuarios');
       },
     });
   }
@@ -47,7 +58,7 @@ export class EmprendimientoComponent implements OnInit{
         this.municipios = data;
       },
       error: (err) => {
-        console.error('Error al obtener los municipios:', err);
+        console.error('Error al consultar los municipios:', err);
       },
     });
   }
@@ -58,7 +69,29 @@ export class EmprendimientoComponent implements OnInit{
         this.categorias = data;
       },
       error: (err) => {
-        console.error('Error al obtener las categorias:', err);
+        console.error('Error al consultar las categorias:', err);
+      },
+    });
+  }
+
+  consultarUsuarios(): void {
+    this.service.consultarUsuarios().subscribe({
+      next: (data) => {
+        this.usuarios = data;
+      },
+      error: (err) => {
+        console.error('Error al consultar los usuarios:', err);
+      },
+    });
+  }
+
+  consultarEventos(): void {
+    this.service.consultarEventos().subscribe({
+      next: (data) => {
+        this.eventos = data;
+      },
+      error: (err) => {
+        console.error('Error al consultar los eventos:', err);
       },
     });
   }
@@ -69,14 +102,16 @@ export class EmprendimientoComponent implements OnInit{
       this.service.crearEmprendimiento(this.selectedEmprendimiento).subscribe({
         next: (nuevoEmprendimiento) => {
           this.emprendimientos.push(nuevoEmprendimiento);
+          this.asignarEmprendimientoUsuario(nuevoEmprendimiento.id);
+          this.asignarEmprendimientoEvento(nuevoEmprendimiento.id);
           form.resetForm();
           this.closeModal();
           console.log('Emprendimiento creado exitosamente:', nuevoEmprendimiento);
-          alert('Emprendimiento creado con éxito');
+          this.swal.success('Éxito', 'Emprendimiento creado con éxito');
         },
         error: (err) => {
           console.error('Error al crear el emprendimiento:', err);
-          alert('Hubo un error al crear el emprendimiento');
+          this.swal.error('Error', 'Hubo un error al crear el emprendimiento');
         },
       });
     }
@@ -88,6 +123,10 @@ export class EmprendimientoComponent implements OnInit{
 
       this.service.editarEmprendimiento(this.selectedEmprendimiento).subscribe({
         next: (emprendimientoEditado) => {
+
+          this.asignarEmprendimientoUsuario(emprendimientoEditado.id);
+          this.asignarEmprendimientoEvento(emprendimientoEditado.id);
+
           const index = this.emprendimientos.findIndex(
             (emp) => emp.id === emprendimientoEditado.id
           );
@@ -96,11 +135,11 @@ export class EmprendimientoComponent implements OnInit{
           }
           this.closeModal();
           console.log('Emprendimiento editado exitosamente:', emprendimientoEditado);
-          alert('Emprendimiento editado con éxito');
+          this.swal.success('Éxito', 'Emprendimiento editado con éxito');
         },
         error: (err) => {
           console.error('Error al editar el emprendimiento:', err);
-          alert('Hubo un error al editar el emprendimiento');
+          this.swal.error('Error', 'Hubo un error al editar el emprendimiento');
         },
       });
     }
@@ -108,28 +147,120 @@ export class EmprendimientoComponent implements OnInit{
 
   // Eliminar un emprendimiento
   deleteEmprendimiento(id: number): void {
-    if (confirm(`¿Estás seguro de que deseas eliminar el emprendimiento con el id ${id}?`)) {
-      this.service.borrarEmprendimiento(id).subscribe({
-        next: () => {
-          this.emprendimientos = this.emprendimientos.filter((emp) => emp.id !== id);
-          alert('Emprendimiento eliminado con éxito');
-          console.log('Emprendimiento eliminado exitosamente');
+    this.swal
+      .confirm(
+        '¿Estás seguro?',
+        `¿Deseas eliminar el emprendimineto con el id ${id}? No podrás revertir esta acción`
+      )
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.service.borrarEmprendimiento(id).subscribe({
+            next: () => {
+              this.emprendimientos = this.emprendimientos.filter((emp) => emp.id !== id);
+              console.log('Emprendimiento eliminado exitosamente');
+              this.swal.success('Éxito', 'Emprendimiento eliminado con éxito');
+            },
+            error: (err) => {
+              console.error('Error al eliminar el emprendimiento:', err);
+              this.swal.error('Error', 'Hubo un error al eliminar el emprendimiento');
+            },
+          });
+        }
+    });
+  }
+
+  asignarEmprendimientoUsuario(id: number): void {
+    const requests = this.selectedUsuarios.map(usuarioId => {
+      const payload = {
+        id_emprendimiento: id,
+        id_usuario: usuarioId,
+      };
+      return this.service.asignarUsuarioEmprendimiento(payload);
+    });
+
+    forkJoin(requests).subscribe({
+      next: (responses) => {
+        console.log('Asignaciones completadas exitosamente:', responses);
+      },
+      error: (err) => {
+        console.error('Error en la asignación de usuarios al emprendimiento:', err);
+      },
+    });
+
+    // this.emprendimientoUsuario.id_emprendimiento = id;
+    //   this.emprendimientoUsuario.id_usuario = this.selectedEmprendimiento.id_usuario;
+    //   this.service.asignarUsuarioEmprendimiento(this.emprendimientoUsuario).subscribe({
+    //     next: (data) => {
+    //       console.log('asignacion del emprendimiento al usuario creado exitosamente:', data);
+    //     },
+    //     error: (err) => {
+    //       console.error('Error en la asignacion del emprendimiento al usuario:', err);
+    //     },
+    // });
+  }
+
+  asignarEmprendimientoEvento(id: number): void {
+
+    const requests = this.selectedEventos.map(eventoId => {
+      const payload = {
+        id_emprendimiento: id,
+        id_evento: eventoId,
+      };
+      return this.service.asignarEmprendimientoEvento(payload);
+    });
+
+    forkJoin(requests).subscribe({
+      next: (responses) => {
+        console.log('Asignaciones completadas exitosamente:', responses);
+      },
+      error: (err) => {
+        console.error('Error en la asignación de usuarios al emprendimiento:', err);
+      },
+    });
+
+    // this.emprendimientoEvento.id_emprendimiento = id;
+    // this.emprendimientoEvento.id_evento = this.selectedEmprendimiento.id_evento;
+    // this.service.asignarEmprendimientoEvento(this.emprendimientoEvento).subscribe({
+    //   next: (data) => {
+    //     console.log('asignacion del emprendimiento al evento creado exitosamente:', data);
+    //   },
+    //   error: (err) => {
+    //     console.error('Error en la asignacion del emprendimiento al evento:', err);
+    //   },
+    // });
+  }
+
+  consultarEmprendimiento(id: number): void {
+      this.service.consultarEmprendimiento(id).subscribe({
+        next: (data) => {
+          console.log('consulta de emprendimiento:', data);
+          if (data.eventosRelacionados && data.eventosRelacionados.length > 0) {
+            data.id_usuario = data.eventosRelacionados[0].id_evento;
+          }
+          if (data.usuariosRelacionados && data.usuariosRelacionados.length > 0) {
+            data.id_evento = data.usuariosRelacionados[0].id_usuario;
+          }
+
+          this.selectedEmprendimiento = { ...data };
+          this.selectedUsuarios = this.selectedEmprendimiento.usuariosRelacionados
+            ? this.selectedEmprendimiento.usuariosRelacionados.map(u => u.id_usuario)
+            : [];
+          this.selectedEventos = this.selectedEmprendimiento.eventosRelacionados
+            ? this.selectedEmprendimiento.eventosRelacionados.map(u => u.id_evento)
+            : [];
         },
         error: (err) => {
           console.error('Error al eliminar el emprendimiento:', err);
-          alert('Hubo un error al eliminar el emprendimiento');
+          this.swal.error('Error', 'Hubo un error al eliminar el emprendimiento');
         },
       });
-    }
   }
 
   // Abre el modal para crear o editar un emprendimiento
   openModal(emprendimiento?: Venture): void {
     if (emprendimiento) {
+      this.consultarEmprendimiento(emprendimiento.id);
       this.modalEditar = true;
-      emprendimiento.id_municipio = emprendimiento.municipio?.id;
-      emprendimiento.id_categoria = emprendimiento.categoria?.id;
-      this.selectedEmprendimiento = { ...emprendimiento };
       this.isModalOpen = true;
     } else {
       this.modalEditar = false;
@@ -142,6 +273,8 @@ export class EmprendimientoComponent implements OnInit{
   closeModal(): void {
     this.isModalOpen = false;
     this.modalEditar = false;
+    this.selectedEventos = [];
+    this.selectedUsuarios = [];
   }
 
 }
